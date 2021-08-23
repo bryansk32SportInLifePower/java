2bf3c9d142bb0584910e86c9fcb8f82b1edcad41
bryansk32SportInLifePower <80155714+bryansk32SportInLifePower@users.noreply.github.com>
2021-08-23 19:55:18 +0300

    LetsCode
    Adventure:Beginner

--- Training/src/main/java/train/guestions/com/Main.java
+++ Training/src/main/java/train/guestions/com/Main.java
@@ -7,11 +7,13 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        String ss;
+        Scanner  s=new Scanner(System.in);
         System.out.println("Vvedute");
-        String ss=s.toString();
+        ss = s.nextLine();
         Train t=new Train();
-        System.out.println(t.detectCapitalUse(ss));
+        System.out.println(ss);
+        System.out.println( t.detectCapitalUse(ss));
     }
 
 }
