2b2b77a1a082282e95c451938fd869cd3c14c60b
bryansk32SportInLifePower <80155714+bryansk32SportInLifePower@users.noreply.github.com>
2021-08-29 19:34:57 +0300

    Massives copy
    Letscode

--- TrainingN/src/main/java/train/question/com/Main.java
+++ TrainingN/src/main/java/train/question/com/Main.java
@@ -1,17 +1,42 @@
 package train.question.com;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        int ss;
-        Scanner  s=new Scanner(System.in);
-        System.out.println("Vvedute");
-        ss = s.nextInt();
-        Train t=new Train();
-        System.out.println(ss);
-        System.out.println( t.detectCapitalUse(ss));
+public static void arrayCheck(int[] array){
+        HashSet<Integer> used = new HashSet<>();
+
+        for(int i = 0; i < array.length; i++){
+        if(used.contains(array[i])){
+        continue;
+        } else {
+        used.add(array[i]);
+        }
+
+        ArrayList<Integer> positions = new ArrayList<>();
+        positions.add(i);
+        for(int j = i + 1; j < array.length; j++){
+            if(array[i] == array[j]){
+        positions.add(j);
+            }
+
+        }
+        System.out.print(array[i] + " in position " );
+        for(Integer p : positions){
+        System.out.print(p + ", ");
+        }
+        System.out.println();
+        }
+        }
+
+        public static void main(String[] args) {
+        Random R = new Random();
+        int a;
+        int[] m = new int [10];
+        for(a = 0; a<m.length; a++) {
+            m[a]=R.nextInt(10);
+            System.out.println(a + " : " + m[a]+" ");
+        }
+        arrayCheck(m);
     }
-}
+    }
+
